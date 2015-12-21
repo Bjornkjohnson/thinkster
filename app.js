6dec23583364e302b0ef8b65a5570c22b9ae9d11
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-12-21 14:21:55 -0500

    add post route and empty controller

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
@@ -42,4 +47,11 @@ function($scope, posts){
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
   };
-}]);
+}])
+.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    
+  }]);
